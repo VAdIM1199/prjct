f52e9f98881e22ded8131fe77ab9a3a6999ebe54
VAdIM1199 <v-dojnikov@mail.ru>
2025-02-28 14:28:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,8 +71,6 @@ save_earth = [
     "Участие в экологических акциях и поддержка организаций, борющихся с изменением климата."
 ]
 photos = [
-    'https://media.istockphoto.com/id/1477849066/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D1%81%D1%83%D1%85%D0%B0-%D0%B8-%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9-%D0%BA%D1%80%D0%B8%D0%B7%D0%B8%D1%81.jpg?s=612x612&w=0&k=20&c=aeNfh_Vh9VvLTiRtW_flmaxa8nIrETRXnfW0cthF5WA=',
-    'https://media.istockphoto.com/id/1333718098/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BB%D0%B5%D1%81%D0%BD%D1%8B%D0%B5-%D0%BF%D0%BE%D0%B6%D0%B0%D1%80%D1%8B-%D0%B2%D1%8B%D0%B7%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BA%D0%BB%D0%B8%D0%BC%D0%B0%D1%82%D0%B0.jpg?s=612x612&w=0&k=20&c=NhSJhpVD1t5F-RI1C9XfBs3l1xsoEfLATpJLk-6eois=',
     'https://naked-science.ru/wp-content/uploads/2019/03/field_image_1422925463_led.jpg'
     'https://ecoportal.info/wp-content/uploads/2016/01/lednik-grinell.jpg'
     'https://otvet.imgsmail.ru/download/20529434_46d31f14e5e1f7ff8fd52b8b7f8affe5_800.jpg'
